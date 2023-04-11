64e872f6d7f77adc866508f553a9e3c6c0ce4849
MICAELA RICCARDI <47574710@ort>
2023-04-11 10:17:55 -0300

    Primera parte hecha

---
+++ Boxeador.cs
@@ -0,0 +1,31 @@
+public class Boxeador{
+    public string nombre{get;set;}
+    private string pais{get;set;}
+    private int peso{get;set;}
+    private int potenciaGolpes{get;set;}
+    private int velocidadPiernas{get;set;}
+
+    public Boxeador(string nom, string pa, int pe, int pg, int vp)
+    {
+        nombre = nom;
+        pais = pa;
+        peso = pe;
+        potenciaGolpes = pg;
+        velocidadPiernas = vp;
+    }
+
+    public double ObtenerSkill()
+    {
+        double suma = velocidadPiernas * 0.6;
+        suma += potenciaGolpes * 0.8;
+        suma += GenerarRandom(1, 10);
+        return suma;
+    }
+    private int GenerarRandom(int min, int max)
+        {
+            int num;
+            Random rnd = new Random();
+            num = rnd.Next(min, max+1);
+            return num;
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿const string OPCION ="Ingrese qué acción quiere hacer: ", NOMBRE = "Ingrese el nombre del boxeador: ", PAIS = "Ingrese país: ", PESO = "Ingrese peso: ", P_GOLPES = "Ingrese la potencia de los golpes: ", V_PIERNAS = "Ingrese la velocidad de las piernas: ";
+//HACER CONSTANTES PARA LOS RANGOS!!!
+string nombre, pais;
+int peso, potenciaGolpes, velocidadPiernas, opcion;
+Boxeador boxeador1, boxeador2;
+
+
+Console.WriteLine("MENSAJE CON OPCIONES :D");
+opcion=IngresarEnteroRango(OPCION, 1, 4);
+
+while (opcion!=4)
+{
+    switch(opcion)
+    {
+        case 1:
+        nombre = IngresarTexto(NOMBRE);
+        pais = IngresarTexto(PAIS);
+        peso = IngresarEntero(PESO);
+        potenciaGolpes = IngresarEnteroRango(P_GOLPES, 1, 100);
+        velocidadPiernas = IngresarEnteroRango(V_PIERNAS, 1, 100);
+
+        boxeador1 = new Boxeador(nombre,pais,peso,potenciaGolpes,velocidadPiernas);
+        break;
+
+        case 2:
+        nombre = IngresarTexto(NOMBRE);
+        pais = IngresarTexto(PAIS);
+        peso = IngresarEntero(PESO);
+        potenciaGolpes = IngresarEnteroRango(P_GOLPES, 1, 100);
+        velocidadPiernas = IngresarEnteroRango(V_PIERNAS, 1, 100);
+
+        boxeador2 = new Boxeador(nombre,pais,peso,potenciaGolpes,velocidadPiernas);
+        break;
+
+        case 3:
+        break;
+    }
+    opcion=IngresarEnteroRango(OPCION, 1, 4);
+
+}
+
+
+int IngresarEntero(string mensaje)
+{
+    Console.WriteLine(mensaje);
+    return int.Parse(Console.ReadLine());
+}
+
+string IngresarTexto(string mensaje)
+{
+    Console.Write(mensaje);
+    return Console.ReadLine();
+}
+
+int IngresarEnteroRango(string mensaje, int desde, int hasta)
+{
+    int entero;
+    do
+    {
+        Console.Write(mensaje);
+        entero = int.Parse(Console.ReadLine());
+    }
+    while (entero < desde || entero > hasta);
+    return entero;
+}
